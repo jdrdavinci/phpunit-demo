2bf00ad9a03236a414e552c84a204491313fa918
Jurn de Ruijter <jderuijter@davinci.nl>
2019-03-20 22:16:30 +0100

    Added car test functions

--- src/Car.php
+++ src/Car.php
@@ -8,11 +8,50 @@
 
 namespace Demo;
 
+/**
+ * Class Car
+ * @package Demo
+ */
 class Car
 {
-    public $model;
     public $brand;
+    public $currentSpeed;
+    public $speedLimit = 100;
 
     const AVAILABLE_BRANDS = ['BMW', 'Audi', 'Volvo'];
+
+    /**
+     * @param int $speed
+     * @return int
+     */
+    public function accelerate($speed = 25) {
+        $this->currentSpeed += $speed;
+
+        if ($this->currentSpeed > $this->speedLimit) {
+            $this->currentSpeed = $this->speedLimit;
+        }
+        return $this->currentSpeed;
+    }
+
+    /**
+     * @param int $speed
+     * @return int
+     */
+    public function brake($speed = 10) {
+        $this->currentSpeed -= $speed;
+
+        if ($this->currentSpeed < 0) {
+            $this->currentSpeed = 0;
+        }
+        return $this->currentSpeed;
+    }
+
+    /**
+     * @return int
+     */
+    public function fullStop() {
+        $this->currentSpeed = 0;
+        return $this->currentSpeed;
+    }
 
 }
--- tests/CarTest.php
+++ tests/CarTest.php
@@ -11,21 +11,51 @@ use Demo\Car;
 
 class CarTest extends TestCase
 {
+    protected $car;
+
+    /**
+     * @before
+     */
+    public function setup() {
+        $this->car = new Car();
+    }
+
     public function testTrueAssertsToTrue() {
         $this->assertTrue(true);
     }
 
     public function testHasPropertyModel() {
-        $this->assertClassHasAttribute("model", "Demo\Car");
+        $this->assertClassHasAttribute("currentSpeed", "Demo\Car");
     }
 
+    public function testAccelerate() {
+        $this->car->accelerate();
+        $this->car->accelerate();
+        $this->car->accelerate();
+        $this->car->accelerate();
+        $this->car->accelerate();
 
-    public function testIfBrandsAreValid() {
-        $brands = ["Audi", "Volvo"];
+        $this->assertEquals(100, $this->car->currentSpeed);
+    }
 
-        foreach ($brands as $brand) {
-            $this->assertContains($brand, Car::AVAILABLE_BRANDS);
-        }
+    public function testBrake() {
+        $this->car->accelerate(250);
+        $this->car->brake(10);
+
+        $this->assertEquals(90, $this->car->currentSpeed);
+    }
+
+    public function testFullStop() {
+        $this->car->accelerate(80);
+        $this->car->fullStop();
+
+        $this->assertEquals(0, $this->car->currentSpeed);
+    }
+
+    public function testThatBrandIsNotValid() {
+        $this->car->brand = 'Ford';
+
+        $this->assertNotContains($this->car->brand, Car::AVAILABLE_BRANDS);
     }
 
 }
